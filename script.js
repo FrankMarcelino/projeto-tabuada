7a94c68ec0ce53f921af2f4c3f7d3c6eb1fe6e36
frankMarcelino <frank.silva.ti@gmail.com>
2023-09-21 13:52:11 -0300

    fim projeto tabuada

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert('ola!')
+function gerar() {
+    let inum = document.getElementById('inum')
+    let res = document.getElementById('ires')
+    
+    if (inum.value.length == 0){
+        window.alert('Digite um número por favor!')
+    } else {
+        let numero = Number(inum.value)
+        res.innerHTML = ''
+        
+        /*let contador =1
+        while (contador <= 10){
+            let item = document.createElement('option')
+            item.text = `${numero} x ${contador} = ${numero * contador}`
+            res.appendChild(item)
+            contador++
+        }*/
+        for(let contador = 1; contador <= 10; contador++ ){
+            let item = document.createElement('option')
+            item.text = `${numero} x ${contador} = ${numero * contador}`
+            res.appendChild(item) 
+        }
+    }
+
+
+   
+}
